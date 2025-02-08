41bf9a834d2f1e24897809f28c7ca75dc529038f
Achmad Fikry <achmadfikryy08@gmail.com>
2025-02-09 02:05:15 +0700

    addition ArrayList

--- src/main/java/CoreJavaBrushUp2.java
+++ src/main/java/CoreJavaBrushUp2.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CoreJavaBrushUp2 {
     public static void main(String[] args) {
         double[] arr2 = {1,2,3.5,4,5,6,7,8,9,10,122};
@@ -15,5 +19,30 @@ public class CoreJavaBrushUp2 {
 
         }
         System.out.println(result);
+
+
+    ArrayList<String> a = new ArrayList<String>();
+    a.add("rahul");
+    a.add("shetty");
+    a.add("academy");
+    a.add("selenium");
+
+    a.remove(2);
+    System.out.println(a.get(0));
+
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println(a.get(i));
+        }
+        System.out.println("********");
+        for (String val : a){
+            System.out.println(val);
+        }
+
+        //item is present in ArrayList
+        System.out.println(a.contains("selenium"));
+        String[] name = {"rahul", "shetty", "academy"};
+        List<String> nameArrayList = Arrays.asList(name); //mengubah array menjadi ArrayList
+        nameArrayList.contains("selenium");
+
     }
 }
