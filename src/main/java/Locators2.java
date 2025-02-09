93695542d0ce90382473c725c0d50c937ba69c03
Achmad Fikry <achmadfikryy08@gmail.com>
2025-02-09 22:18:05 +0700

    addition of using dynamic string based on text displayed on UI

--- src/main/java/Locators2.java
+++ src/main/java/Locators2.java
@@ -15,9 +15,11 @@ public class Locators2 {
         //implicit wait, it's for waiting something to show (no element found error)
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
+        String password = getPassword(driver);
+
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys(name);
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
         driver.findElement(By.className("signInBtn")).click();
         Thread.sleep(1000); //explicit wait, it's already there but still not showing because still not in stable state (intercept error)
         System.out.println(driver.findElement(By.tagName("p")).getText());
@@ -26,5 +28,26 @@ public class Locators2 {
 
         driver.findElement(By.xpath("//*[text()='Log Out']")).click();
         driver.close();
+    }
+
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+        //Please use temporary password 'rahulshettyacademy' to Login.
+
+        String[] passwordArray = passwordText.split("'");
+        //0th index : Please use temporary password
+        //1st index : rahulshettyacademy' to Login.
+
+//        String[] passwordArray2 = passwordArray[1].split("'");
+//        String password = passwordArray2[0];
+        String password = passwordArray[1].split("'")[0];
+        //0th index : rahulshettyacademy
+        //1st index : to Login.
+
+        return password;
     }
 }
