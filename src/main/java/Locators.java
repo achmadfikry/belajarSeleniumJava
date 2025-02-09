06c16e80e4dd14a9165ef309df254328b8983cfb
Achmad Fikry <achmadfikryy08@gmail.com>
2025-02-09 13:42:12 +0700

    addition find element by xpath using parent child index, find by element by css selector using class & parent child tag, explicit wait

--- src/main/java/Locators.java
+++ src/main/java/Locators.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Locators {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:/Users/Lenovo/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        //implicit wait
+        //implicit wait, it's for waiting something to show (no element found error)
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
@@ -19,10 +19,15 @@ public class Locators {
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 
         driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000); //explicit wait, it's already there but still not showing because still not in stable state (intercept error)
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
         driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@gmail.com");
         driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
         driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("john@gmail.com");
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("085214871547");
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        driver.findElement(By.cssSelector("form p")).click();
+        System.out.println(driver.findElement(By.cssSelector("form p")).getText());
 
     }
 }
