0933cc9763c89b199e158ce6c645e1db7e3c904e
Achmad Fikry <achmadfikryy08@gmail.com>
2025-02-15 10:27:30 +0700

    Assignement 2

---
+++ src/main/java/Assignment2.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        driver.findElement(By.name("name")).sendKeys("Rahul");
+    }
+}
