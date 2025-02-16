744a76086864fc295934c9340728f748f01c4e52
Achmad Fikry <achmadfikryy08@gmail.com>
2025-02-16 17:58:57 +0700

    addition refactor code, addition implicit & explicit wait

--- src/main/java/base.java
+++ src/main/java/base.java
@@ -2,21 +2,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class base {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-
-        String [] itemsNeeded = {"Cucumber", "Brocolli"};
-        int j = 0;
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
         Thread.sleep(3000);
+
+        String [] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot"};
+        //kl pake static bisa langsung, tapi kl gak pake static harus intance kelas nya dulu
+        //addItems(driver, itemsNeeded);
+        base b = new base();
+        b.addItems(driver, itemsNeeded);
+
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.xpath("//button[contains (text(), 'PROCEED TO CHECKOUT')]")).click();
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='promoCode']")));
+        driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.xpath("//button[@class='promoBtn']")).click();
+        //explicit wait
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='promoInfo']")));
+        Assert.assertTrue(driver.findElement(By.xpath("//span[@class='promoInfo']")).getText().contains("Code applied"));
+    }
+
+    public void addItems(WebDriver driver, String[] itemsNeeded) {
+        int j = 0;
         List<WebElement> products = driver.findElements(By.cssSelector("h4[class='product-name']"));
 //        for (WebElement product : products) {} //gak bisa pake ini karena harus pake index yang sama
         for (int i = 0; i < products.size(); i++) {
@@ -39,5 +61,6 @@ public class base {
                 }
             }
         }
+
     }
 }
