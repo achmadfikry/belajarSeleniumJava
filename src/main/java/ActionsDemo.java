29ecd1f85243787e99d54baa59f186e241c33299
Achmad Fikry <achmadfikryy08@gmail.com>
2025-02-19 22:59:30 +0700

    addition Actions, hover, keyDown Shift, Hello

--- src/main/java/ActionsDemo.java
+++ src/main/java/ActionsDemo.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,9 +16,12 @@ public class ActionsDemo {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.get("https://www.amazon.com/");
 
-
         Actions actions = new Actions(driver);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='nav-link-accountList']")));
+        actions.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
         actions.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).build().perform();
+
+        WebElement move = driver.findElement(By.xpath("//a[@id='nav-link-accountList']"));
+        actions.moveToElement(move).contextClick().build().perform();
+
     }
 }
