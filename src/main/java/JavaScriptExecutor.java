b1c51be594cb5cb4e9c6c382a51706e96ccc7db9
Achmad Fikry <achmadfikryy08@gmail.com>
2025-05-16 23:16:17 +0700

    addition of sum up the values in the table and compare it with total

--- src/main/java/JavaScriptExecutor.java
+++ src/main/java/JavaScriptExecutor.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class JavaScriptExecutor {
     public static void main(String[] args) throws InterruptedException {
@@ -17,5 +22,20 @@ public class JavaScriptExecutor {
         //DOM: document.querySelector(".class")
         Thread.sleep(3000);
         js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+
+        //xpath: //td[4]
+        //css: td:nth-child(4)
+        List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+        int sum = 0;
+        for (int i = 0; i < values.size(); i++) {
+            System.out.println(Integer.parseInt(values.get(i).getText()));
+            sum = sum + Integer.parseInt(values.get(i).getText());
+            System.out.println(sum);
+        }
+
+        int total = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
+        System.out.println(total);
+
+        Assert.assertEquals(sum, total);
     }
 }
