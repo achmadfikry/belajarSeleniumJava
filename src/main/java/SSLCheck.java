d989df00e665d4e1b65b34c086df26fd744cbd82
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-06-17 14:57:06 +0700

    Addition set default download directory

--- src/main/java/SSLCheck.java
+++ src/main/java/SSLCheck.java
@@ -3,13 +3,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SSLCheck {
     public static void main(String[] args) {
         ChromeOptions options = new ChromeOptions();
         options.setAcceptInsecureCerts(true);
+
         Proxy proxy = new Proxy();
         proxy.setHttpProxy("ipaddress:4444");
         options.setCapability("proxy", proxy);
+
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("download.default_directory", "/directory/path");
+        options.setExperimentalOption("prefs", prefs);
+
         WebDriver driver = new ChromeDriver(options); //intimating options to chrome driver
         driver.manage().window().maximize();
         driver.get("https://expired.badssl.com/");
