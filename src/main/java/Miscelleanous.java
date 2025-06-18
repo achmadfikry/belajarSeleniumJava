7d813cada7cd082f91c25cbe598a920b100e5a4c
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-06-18 21:25:08 +0700

    Addition screenshot on selenium

--- src/main/java/Miscelleanous.java
+++ src/main/java/Miscelleanous.java
@@ -1,8 +1,14 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Miscelleanous {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
@@ -12,5 +18,10 @@ public class Miscelleanous {
         //login page- verify login url
         driver.get("https://www.google.com");
 
+
+        //delete screenshot & kill the process which is running in the background
+        //convert webdriver object into screenshot object
+        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); //casting driver object to TakesScreenshot object
+        FileUtils.copyFile(src, new File("/Users/telkomdev/Redis/Fikry/screenshot.png")); //rewrite source object to our local machine
     }
 }
