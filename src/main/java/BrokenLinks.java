18ab101af161355458def905edf3b0c319a2491a
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-06-19 11:19:59 +0700

    Additio iterate all links to validate broken link

--- src/main/java/BrokenLinks.java
+++ src/main/java/BrokenLinks.java
@@ -1,12 +1,15 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 
 public class BrokenLinks    {
     public static void main(String[] args) throws MalformedURLException, IOException {
@@ -14,17 +17,28 @@ public class BrokenLinks    {
         driver.manage().window().maximize();
         driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
 
-        //broken url
-        //step 1 : to get all  urls tied up to the links using selenium
-        //step 2 : Java methods will call url's & gets you the status code
-        // step 3 : if status code > 400 then that url is not working -> link which tied up to url is broken
-        //a[href*="https://www.soapui.org/"]
-        String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href"); //step 1
+        List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+        for (WebElement link : links) {
+//            link.getAttribute("href");
+            //broken url
+            //step 1 : to get all  urls tied up to the links using selenium
+            //step 2 : Java methods will call url's & gets you the status code
+            // step 3 : if status code > 400 then that url is not working -> link which tied up to url is broken
+            //a[href*="https://www.soapui.org/"]
+            String url = link.getAttribute("href"); //step 1
 
-        HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
-        conn.setRequestMethod("HEAD");
-        conn.connect();
-        int respCodde = conn.getResponseCode();
-        System.out.println(respCodde);
+            HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            int respCode = conn.getResponseCode();
+            System.out.println(respCode);
+
+            if (respCode > 400) {
+                System.out.println("The link with the text " + link.getText() + " is broken with code " + respCode);
+                Assert.assertTrue(false);
+            }
+
+        }
+
     }
 }
