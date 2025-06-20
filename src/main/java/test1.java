2030cb50a7208521fb88020aceac3f735079f5ed
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-06-20 16:17:08 +0700

    Addition match on java stream

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -69,16 +70,21 @@ public class test1 {
 
         //print names which has last letter as "a" with uppercase
         // "Abhijeet", "Don", "Alekhya", "Adam", "Rama" is array
-        Stream.of("Abhijeet", "Don", "Alekhya", "Adam", "Rama").filter(s -> s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+//        Stream.of("Abhijeet", "Don", "Alekhya", "Adam", "Rama").filter(s -> s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
 
         //print names which start letter as "a" with uppercase and sorted
         //convert array to array list
         List<String> names1 = Arrays.asList("Azbhijeet", "Don", "Alekhya", "Adam", "Rama");
-        names1.stream().filter(s->s.startsWith("A")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+//        names1.stream().filter(s->s.startsWith("A")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
 
         //merge two array list
         Stream<String> newStream = Stream.concat(names.stream(), names1.stream());
-        newStream.sorted().forEach(s->System.out.println(s));
+//        newStream.sorted().forEach(s->System.out.println(s));
+
+        //match: check wether Adam is in the list or not, if present return true, if it is not return false
+         boolean flag = newStream.anyMatch(s -> s.equalsIgnoreCase("Adam"));
+        System.out.println(flag);
+        Assert.assertTrue(flag);
 
     }
 }
